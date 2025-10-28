196c88d4ae459e7c5e2732dfaf41a5f27445283e
David Garcia Yarce <davidgar048@gmail.com>
2025-10-28 08:18:04 -0500

    feat(root): :sparkles: Root

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,7 +1,12 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { AppService } from './app.service';
 
-@Controller()
+@Controller('api')
 export class AppController {
-  constructor() {}
+  constructor(private readonly appService: AppService) {}
 
+  @Post('response')
+  createResponse(){
+    
+  }
 }
