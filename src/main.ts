e9eb08d5fb4ab27f47ecd04dab0e40536025e26b
David Garcia Yarce <davidgar048@gmail.com>
2025-10-30 19:14:35 -0500

    feat: :sparkles: cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { Logger } from '@nestjs/common';
 async function bootstrap() {
   const logger = new Logger('Bootstrap');
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+  origin: [
+    process.env.FRONTEND_URL,
+  ],
+  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization'],
+});
   await app.listen(process.env.PORT || 3000);
   logger.log('Application is running on: ' + (await app.getUrl()));
 }
